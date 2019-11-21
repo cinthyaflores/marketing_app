47607e4524320087e8558c263751c849f44ffba5
Myrka Larios <myrka@michelada.io>
2019-11-21 10:28:07 -0600

    TODO: cleanup logic in tasks

--- app/controllers/my_tasks_controller.rb
+++ app/controllers/my_tasks_controller.rb
@@ -1,26 +1,14 @@
 # frozen_string_literal: true
 
 class MyTasksController < ApplicationController
-  before_action :assign_task, only: %i[show edit update]
+  before_action :assign_task, only: %i[show edit]
   def index
     @tasks = Task.by_user(current_user.id)
-  end
-
-  def update
-    if @task.update(task_params)
-      redirect_to my_tasks_path(@post), notice: 'Task was successfully updated.'
-    else
-      redirect_to my_tasks_path(@post), error: 'Task was unsuccessfully updated.'
-    end
   end
 
   private
 
   def assign_task
     @task = Task.find(params[:id])
-  end
-
-  def task_params
-    params.require(:task).permit(:content, :visual)
   end
 end
