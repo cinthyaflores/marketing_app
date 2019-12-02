f184f7f808f1c94c41032d9c71ea5cdc47cc896b
Myrka Larios <myrka@michelada.io>
2019-12-01 20:31:48 -0600

    Add history and download charts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!, if: :not_root
+  before_action :log
+
+  private
+
+  def not_root
+    params[:controller] != 'landing_page'
+  end
+
+  def log
+    History.create(content: params.to_s, user: current_user.inspect.to_s)
+  end
 end
